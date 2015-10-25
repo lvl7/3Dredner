b7cb3c66f19b8b47e306f583d8c4a726a19b4adc
lvl7 <lvl7gg@gmail.com>
2015-10-25 12:58:54 +0100

    set up directory structure.

---
+++ src/world/World.cpp
@@ -0,0 +1,14 @@
+/*
+ * World.cpp
+ *
+ *  Created on: 23-10-2015
+ *      Author: lvl7
+ */
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+	std::cout << "Hello World";
+	return 0;
+}
