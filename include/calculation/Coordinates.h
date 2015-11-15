1c7968fd90e15022c91a7a887c839a7bd6cd18b6
Ania Matloch <matloch.ania@gmail.com>
2015-11-15 12:13:47 +0100

    Coordinates

---
+++ include/calculation/Coordinates.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+struct Coordinates {
+    double x, y, z;
+    Coordinates(double x_param, double y_param, double z_param): x(x_param), y(y_param), z(z_param) {};
+    
+};
--- include/calculation/Points.h
+++ include/calculation/Points.h
@@ -1,14 +1,10 @@
 #pragma once
 
+#include <calculation/Coordinates.h>
 #include <vector>
 #include <iostream>
 #include <string>
 
-struct Coordinates {
-    double x, y, z;
-    Coordinates(double x_param, double y_param, double z_param): x(x_param), y(y_param), z(z_param) {};
-
-};
 
 class Points {
     std::vector<Coordinates> points;
